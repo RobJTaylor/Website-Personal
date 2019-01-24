9d97cbdae8cfb2590778106daa6127df4975ebe7
Rob Taylor <robert.j.taylor117@gmail.com>
2019-01-24 22:00:37 +0000

    Bars will only animate once

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,9 +1,29 @@
 var skills = ['Java', 'C#', 'JavaScript', 'PHP', 'HTML', 'CSS', 'Python'];
 var count = 0;
+var animated = 0;
 
 function changeSkill() {
     setInterval(updateText, 3000);
 
+    if (animated == 0) {
+        animateBars();
+    }
+}
+
+function updateText() {
+    $("#mainSkill").fadeOut('fast', function () {
+        $("#mainSkill").html("<mark>" + skills[count] + "</mark>");
+        $("#mainSkill").fadeIn('fast');
+    });
+
+    count++;
+
+    if (count == skills.length) {
+        count = 0;
+    }
+}
+
+function animateBars() {
     $("#androidDevelopment").animate({
         width: "75%"
     }, 1500)
@@ -19,17 +39,6 @@ function changeSkill() {
     $("#developmentMethodologies").animate({
         width: "90%"
     }, 1500)
-}
 
-function updateText() {
-    $("#mainSkill").fadeOut('fast', function () {
-        $("#mainSkill").html("<mark>" + skills[count] + "</mark>");
-        $("#mainSkill").fadeIn('fast');
-    });
-
-    count++;
-
-    if (count == skills.length) {
-        count = 0;
-    }
+    animated = 1;
 }
