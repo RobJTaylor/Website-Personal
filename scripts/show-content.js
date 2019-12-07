ab9af76b6cf69492bf44b68434ab38c8ba29dc9b
Rob Taylor <robert.j.taylor117@gmail.com>
2019-12-07 16:53:51 +0000

    Reverted showContent, added new active style for nav links

--- scripts/show-content.js
+++ scripts/show-content.js
@@ -1,54 +1,53 @@
 var currentContent = "#about";
+var lastLink = "about-link";
 
-function showContent(id) {
-    elementIds = ['about', 'skills', 'industry', 'projects', 'contact'];
+function showContent(id) {    
 
-    if (id == "websites" || id == "software") {
+    $("#" + lastLink).removeClass('nav-active');
+    $(event.target).addClass('nav-active');
+    lastLink = $(event.target).attr('id');
+    console.log(lastLink);
 
-    } else {
-        $(currentContent).fadeOut('fast', function () {
-            var newContent = $("#" + id);
-            newContent.addClass("a:active");
-            newContent.show();
-            $("#" + id).fadeIn('fast');
-        });
+    $(currentContent).fadeOut('fast', function () {
+        var newContent = $("#" + id);
+        newContent.addClass("a:active");
+        newContent.show();
+        $("#" + id).fadeIn('fast');
+    });
 
-        currentContent = "#" + id;
-        var textElement = $("#headerText");
-
-        switch (id) {
-            case "about":
-                $("#headerText").fadeOut('fast', function () {
-                    $("#headerText").html("Hi, I'm Rob Taylor");
-                    $("#headerText").fadeIn('fast');
-                });
-                break;
-            case "skills":
-                $("#headerText").fadeOut('fast', function () {
-                    $("#headerText").html("My Skills");
-                    $("#headerText").fadeIn('fast');
-                });
-                changeSkill();
-                break;
-            case "industry":
-                $("#headerText").fadeOut('fast', function () {
-                    $("#headerText").html("Industry Experience");
-                    $("#headerText").fadeIn('fast');
-                });
-                break;
-            case "projects":
-                $("#headerText").fadeOut('fast', function () {
-                    $("#headerText").html("Projects");
-                    $("#headerText").fadeIn('fast');
-                });
-                break;
-            case "contact":
-                $("#headerText").fadeOut('fast', function () {
-                    $("#headerText").html("Contact Me");
-                    $("#headerText").fadeIn('fast');
-                });
-                break;
-        }
+    currentContent = "#" + id;
+    switch (id) {
+        case "about":
+            $("#headerText").fadeOut('fast', function () {
+                $("#headerText").html("Hi, I'm Rob Taylor");
+                $("#headerText").fadeIn('fast');
+            });
+            break;
+        case "skills":
+            $("#headerText").fadeOut('fast', function () {
+                $("#headerText").html("My Skills");
+                $("#headerText").fadeIn('fast');
+            });
+            changeSkill();
+            break;
+        case "industry":
+            $("#headerText").fadeOut('fast', function () {
+                $("#headerText").html("Industry Experience");
+                $("#headerText").fadeIn('fast');
+            });
+            break;
+        case "projects":
+            $("#headerText").fadeOut('fast', function () {
+                $("#headerText").html("Projects");
+                $("#headerText").fadeIn('fast');
+            });
+            break;
+        case "contact":
+            $("#headerText").fadeOut('fast', function () {
+                $("#headerText").html("Contact Me");
+                $("#headerText").fadeIn('fast');
+            });
+            break;
     }
 }
 
